2e72af307bbcc1649b267717364a4466edf862e7
GabrielMello159 <gabrielmatheusmello_pbl@hotmail.com>
2025-11-03 03:25:32 -0300

    pagina register

--- src/presenca.js
+++ src/presenca.js
@@ -1,4 +1,32 @@
-const dominio = "";
+const dominio = "https://chamada-api.azurewebsites.net/";
+
+    async function registrarUsuario() {
+      const nome = document.getElementById("nome").value;
+      const email = document.getElementById("email").value;
+      const senha = document.getElementById("senha").value;
+
+      if (!nome || !email || !senha) {
+        return alert("Preencha todos os campos!");
+      }
+
+      try {
+        const response = await fetch("https://chamada-api.azurewebsites.net/auth/register", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ nome, email, senha })
+        });
+
+        if (response.ok) {
+          alert("Cadastro realizado com sucesso!");
+          window.location.href = "login.html";
+        } else {
+          const erro = await response.json();
+          alert("Erro ao cadastrar: " + erro.message);
+        }
+      } catch (err) {
+        alert("Erro de conexão: " + err.message);
+      }
+    }
 
 async function login() {
   const email = document.getElementById("email").value;
@@ -16,7 +44,6 @@ async function login() {
   localStorage.setItem("token", data.token);
   alert("Login realizado com sucesso!");
   window.location.href = "presenca.html";
-
 }
 
 async function registrarPresenca() {
@@ -40,23 +67,8 @@ async function registrarPresenca() {
   if (response.ok) alert("Presença registrada!");
   else alert("Erro ao registrar presença.");
 
-  await obterPresencas(); // Atualiza tabela
-};
-
-  const response = await fetch(`${dominio}/presenca`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`
-    },
-    body: JSON.stringify(presenca)
-  });
-
-  if (response.ok) alert("Presença registrada!");
-  else alert("Erro ao registrar presença.");
-
-  await obterPresencas(); // Atualiza tabela
-
+  obterPresencas(); 
+}
 
 async function obterPresencas() {
   const token = localStorage.getItem("token");
@@ -115,3 +127,4 @@ function logout() {
   localStorage.removeItem("token");
   window.location.href = "index.html";
 }
+
